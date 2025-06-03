08ec9aa696cd910e3f962b4c5b7814728e8f2867
JRRJ1809 <jrvitbort@gmail.com>
2025-06-03 15:24:57 -0300

    Login ADM

--- src/pages/Adm/Adm.jsx
+++ src/pages/Adm/Adm.jsx
@@ -1,28 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../../assets/senai-logo.png';
 import './Adm.css';
 
+const urlAdm = 'http://localhost:4000/adm';
+
 const Adm = () => {
+  const [login, setLogin] = useState('');
+  const [senha, setSenha] = useState('');
+  const [tipo, setTipo] = useState('');
+  const [erro, setErro] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      const response = await fetch(`${urlAdm}?login=${login}&senha=${senha}`);
+      const data = await response.json();
+
+      if (data.length > 0) {
+        alert('Login bem-sucedido!');
+       
+      } else {
+        setErro('Usuário ou senha inválidos');
+      }
+    } catch (err) {
+      console.error('Erro ao conectar com o servidor:', err);
+      setErro('Erro de conexão');
+    }
+  };
+
   return (
     <div className="adm-container">
       <div className="adm-box">
         <div className="login-boxx">
-                <img src={logo} alt="Logo SENAI" className="logo" />
-        <h2>Login do Administrador</h2>
+          <img src={logo} alt="Logo SENAI" className="logo" />
+          <h2>Login do Administrador</h2>
 
-        <div className="form-group">
-          <label>Login</label>
-          <input type="text" placeholder="Usuário do ADM" />
+          <div className="form-group">
+            <label>Login</label>
+            <input
+              type="text"
+              placeholder="Usuário do ADM"
+              value={login}
+              onChange={(e) => setLogin(e.target.value)}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Senha</label>
+            <input
+              type="password"
+              placeholder="Senha"
+              value={senha}
+              onChange={(e) => setSenha(e.target.value)}
+            />
+          </div>
+
+          {erro && <p style={{ color: 'red' }}>{erro}</p>}
+
+          <button className="btn" onClick={handleLogin}>Entrar</button>
         </div>
-
-        <div className="form-group">
-          <label>Senha</label>
-          <input type="password" placeholder="Senha" />
-        </div>
-
-        <button className="btn">Entrar</button>
       </div>
-    </div>
     </div>
   );
 };
